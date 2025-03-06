98258e0d5dd3da495bbbf437f2754fb68c7f6b5f
SuardHila <suardhila9@gmail.com>
2025-03-06 08:31:38 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,20 @@
+/*
+Create datbases and schemas ;
+Script purpouse:
+Create a new database and new schemas
+
+*/
+
+
+USE master;
+
+CREATE DATABASE DataWareHouse;
+
+USE DataWareHouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
